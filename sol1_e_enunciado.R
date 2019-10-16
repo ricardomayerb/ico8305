c6fe436c00d50c7fec1f8fdb11221bd08b5e0915
ricardomayerb <ricardomayerb@gmail.com>
2019-10-16 15:27:40 -0300

    pts

--- sol1_e_enunciado.R
+++ sol1_e_enunciado.R
@@ -2,22 +2,22 @@
 library(readxl)
 
 # 1. Use la función read_excel para cargar los datos que se encuentran en el archivo excel
-#  datos_ie.xlsx
+#  datos_ie.xlsx (0 pts)
 misdatos <- read_excel("datos_e.xlsx")
 
 
 
 # 2. Para las variables "pib_per_capita" y "esperanza_de_vida" compute lo siguiente
-# 2.1 la media cada una
-# 2.2 la desviación estandar de cada una
-# 2.3 la cantidad de observaciones (n) de cada una
+# 2.1 la media cada una (2 pts)
+# 2.2 la desviación estandar de cada una (2 pts)
+# 2.3 la cantidad de observaciones (n) de cada una (2 pts)
 
 
 
 
 
 # 3. Grafique los histogramas de estas tres variables:
-# "pib_per_capita", "esperanza_de_vida" y el logaritmo natural de la variable "población".
+# "pib_per_capita" (1 pto), "esperanza_de_vida" (1 pto) y el logaritmo natural de la variable "población" (2 pts).
 # Puede usar cualquier función y paquete de R que grafique histogramas
 
 
